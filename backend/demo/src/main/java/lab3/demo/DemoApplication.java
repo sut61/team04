ca92a4374c9580809f35ab180427cd8e0cae285c
mossthirawuth <moss.show@gmail.com>
2019-01-18 18:09:51 +0700

    แก้ไข code ไฟล์ DemoApplication - close#52

--- backend/demo/src/main/java/lab3/demo/DemoApplication.java
+++ backend/demo/src/main/java/lab3/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package lab3.demo;
 
+import lab3.demo.Entity.*;
+import lab3.demo.Repository.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import java.util.stream.Stream;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,6 +15,34 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@Bean
+	ApplicationRunner init(CarTypeRepository repository,
+							GenderRepository genderRepository,
+							ProvinceRepository provinceRepository) {
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+				CarType car = new CarType();
+				car.setName(name);
+				repository.save(car);
+			});
+			repository.findAll().forEach(System.out::println);
 
+			Stream.of("Men", "Women").forEach(name -> {
+				Gender gender = new Gender();
+				gender.setSex(name);
+				genderRepository.save(gender);
+			});
+			genderRepository.findAll().forEach(System.out::println);
+
+			Stream.of("จังหวัดเชียงใหม่", "จังหวัดเชียงราย", "จังหวัดลำปาง", "จังหวัดลำพูน", "จังหวัดแม่ฮ่องสอน",
+					"จังหวัดน่าน", "จังหวัดพะเยา", "จังหวัดแพร่", "จังหวัดอุตรดิตถ์").forEach(name -> {
+				Province province = new Province();
+				province.setName(name);
+				provinceRepository.save(province);
+			});
+			provinceRepository.findAll().forEach(System.out::println);
+		};
+	}
 }
 

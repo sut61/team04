f61f4fcba125b5a0b1cc3fc75b35391faaae76f0
anuttapon <balck_star1@hotmail.com>
2019-02-07 20:34:30 +0700

    เพิ่ท Entity PriceType - close #99

---
+++ backend/demo/src/main/java/lab3/demo/Entity/PriceType.java
@@ -0,0 +1,23 @@
+package lab3.demo.Entity;
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+
+public class PriceType {
+  @Id @GeneratedValue
+    private Long id;
+
+    String priceType;
+ 
+
+}

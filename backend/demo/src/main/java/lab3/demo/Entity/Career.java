a971063fdbefd7333920fa7a53bf26081ddfc59b
nicecpe2105 <channarong_seeho@hotmail.com>
2019-02-08 20:56:12 +0700

    add entity Career - close #106

---
+++ backend/demo/src/main/java/lab3/demo/Entity/Career.java
@@ -0,0 +1,25 @@
+package lab3.demo.Entity;
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+
+public class Career {
+    @Id @GeneratedValue
+    private Long id;
+
+    String name;
+
+
+
+
+}

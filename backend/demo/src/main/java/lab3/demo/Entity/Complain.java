56c829cf96dd7b4a7e9b7d29acd09e5469e0bd12
mossthirawuth <moss.show@gmail.com>
2019-02-09 20:39:28 +0700

    เพิ่มตัวดักจับใน Entity Complain -close#142

--- backend/demo/src/main/java/lab3/demo/Entity/Complain.java
+++ backend/demo/src/main/java/lab3/demo/Entity/Complain.java
@@ -1,7 +1,14 @@
 package lab3.demo.Entity;
 
 import lombok.*;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -23,6 +30,8 @@ public class Complain {
     @ManyToOne
     private DriverTaxi driverTaxi;
 
+     @Size(min =5,max=20 ) @NotNull
+    @Pattern(regexp ="[\\w\\s]+")
     private @NonNull String message;
     
 

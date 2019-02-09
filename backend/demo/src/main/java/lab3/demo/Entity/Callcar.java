e8d97585c452a16c26e8c7f0a5ea82d640302d5e
sarochmongkoldon <saroch_na555@hotmail.com>
2019-02-09 21:45:56 +0700

    Edit DemoApplicationTests- close #144

--- backend/demo/src/main/java/lab3/demo/Entity/Callcar.java
+++ backend/demo/src/main/java/lab3/demo/Entity/Callcar.java
@@ -5,6 +5,9 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Getter
@@ -17,7 +20,12 @@ public class Callcar {
     @Id @GeneratedValue
     private Long id;
 
+    @NotNull
+    @Size(min=10, max=15)
     String dest;
+
+    @NotNull
+    @Size(min=10, max=15)
     String cur;
 
     @ManyToOne

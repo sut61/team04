147aba9446a683c03908ca66266150f174ac002a
anuttapon <balck_star1@hotmail.com>
2019-02-10 01:44:09 +0700

    add menudriver - close #155

---
+++ frontend/Car/src/app/menudriver/menudriver.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menudriver',
+  templateUrl: './menudriver.component.html',
+  styleUrls: ['./menudriver.component.css']
+})
+export class MenudriverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

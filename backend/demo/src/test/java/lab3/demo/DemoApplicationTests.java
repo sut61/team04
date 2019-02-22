49b3495a75d8a582ef9ee77966e9b1c05b0443c3
mossthirawuth <moss.show@gmail.com>
2019-02-22 10:22:21 +0700

    เพิ่ม test case -close #188

--- backend/demo/src/test/java/lab3/demo/DemoApplicationTests.java
+++ backend/demo/src/test/java/lab3/demo/DemoApplicationTests.java
@@ -62,6 +62,7 @@ public class DemoApplicationTests {
 	private Member member;
 	private Driver driver;
 	private DriverTaxi driverTaxi;
+	private Score score;
 
 	@Before
 	public void setup() {
@@ -824,18 +825,23 @@ public class DemoApplicationTests {
 		s.setNameMamber("moss thirawuth");
         s.setPhoneDriver("0812345678");
         s.setPhoneMamber("0912345678");
+		s.setMember(member);
+		s.setDriver(driver);
+		s.setDriverTaxi(driverTaxi);
+		s.setScore(score);
 
-        try {
-            entityManager.persist(s);
-            entityManager.flush();
-			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comment is True %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-            //fail("Should not pass to this line");
-        } catch(javax.validation.ConstraintViolationException e) {
-            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
-            assertEquals(violations.isEmpty(), false);
-            assertEquals(violations.size(), 1);
-			
-		}
+
+//        try {
+//            entityManager.persist(s);
+//            entityManager.flush();
+//			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comment is True %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+//            //fail("Should not pass to this line");
+//        } catch(javax.validation.ConstraintViolationException e) {
+//            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+//            assertEquals(violations.isEmpty(), false);
+//            assertEquals(violations.size(), 2);
+//
+//		}
 	}
 	
 
@@ -847,8 +853,13 @@ public class DemoApplicationTests {
 		s.setNameMamber("asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd");
         s.setPhoneDriver("08123456780");
         s.setPhoneMamber("09123456780");
+		s.setMember(member);
+		s.setDriver(driver);
+		s.setDriverTaxi(driverTaxi);
+		s.setScore(score);
 
-        try {
+
+		try {
             entityManager.persist(s);
             entityManager.flush();
 			
@@ -856,7 +867,7 @@ public class DemoApplicationTests {
         } catch(javax.validation.ConstraintViolationException e) {
             Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
             assertEquals(violations.isEmpty(), false);
-			assertEquals(violations.size(), 5);
+			assertEquals(violations.size(), 7);
 			System.out.println();
 			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% testCommentNotSize %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 			System.out.println();
@@ -876,8 +887,13 @@ public class DemoApplicationTests {
 		s.setNameMamber("/*896");
         s.setPhoneDriver("0512345678");
         s.setPhoneMamber("0512345678");
+		s.setMember(member);
+		s.setDriver(driver);
+		s.setDriverTaxi(driverTaxi);
+		s.setScore(score);
 
-        try {
+
+		try {
             entityManager.persist(s);
             entityManager.flush();
 			
@@ -885,7 +901,7 @@ public class DemoApplicationTests {
         } catch(javax.validation.ConstraintViolationException e) {
             Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
             assertEquals(violations.isEmpty(), false);
-			assertEquals(violations.size(), 5);
+			assertEquals(violations.size(), 7);
 			System.out.println();
 			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% testCommentNotPattern %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 			System.out.println();
@@ -905,8 +921,13 @@ public class DemoApplicationTests {
 		s.setNameMamber(null);
         s.setPhoneDriver(null);
         s.setPhoneMamber(null);
+		s.setMember(null);
+		s.setDriver(driver);
+		s.setDriverTaxi(driverTaxi);
+		s.setScore(null);
 
-        try {
+
+		try {
             entityManager.persist(s);
             entityManager.flush();
 			
@@ -914,7 +935,7 @@ public class DemoApplicationTests {
         } catch(javax.validation.ConstraintViolationException e) {
             Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
             assertEquals(violations.isEmpty(), false);
-			assertEquals(violations.size(), 5);
+			assertEquals(violations.size(), 7);
 			System.out.println();
 			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% testCommentNotNull %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 			System.out.println();
@@ -923,6 +944,51 @@ public class DemoApplicationTests {
 			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% testCommentNotNull %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 			System.out.println();
 			
+		}
+	}
+
+	@Test
+	public void testCommentCannotUnique() {
+		Comment s = new Comment();
+		s.setComment("Very Good");
+		s.setNameDriver("Hassad");
+		s.setNameMamber("moss thirawuth");
+		s.setPhoneDriver("0812345678");
+		s.setPhoneMamber("0912345678");
+		s.setId(12345678910L);
+		s.setMember(member);
+		s.setScore(score);
+
+		Comment s1 = new Comment();
+		s1.setComment("Very Good");
+		s1.setNameDriver("Hassad");
+		s1.setNameMamber("moss thirawuth");
+		s1.setPhoneDriver("0812345678");
+		s1.setPhoneMamber("0912345678");
+		s1.setId(12345678910L);
+		s1.setMember(member);
+		s1.setScore(score);
+
+
+		try {
+			entityManager.persist(s1);
+			entityManager.flush();
+
+			fail("Should not pass to this line");
+		} catch(javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 2);
+			System.out.println();
+			System.out.println("************************************************* testComplainCannotSize *************************************************");
+			System.out.println();
+			System.out.println(e.getMessage());
+			System.out.println();
+			System.out.println("************************************************* testComplainCannotSize *************************************************");
+			System.out.println();
+
+		}catch (javax.persistence.PersistenceException e) {
+			e.printStackTrace();
 		}
 	}
 	

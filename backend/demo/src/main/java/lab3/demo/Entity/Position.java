fcd7766f44b81f8dc1a25c3c5e887fc11531a77a
jirayut1233 <jirayoot_19pop@hotmail.com>
2019-03-07 22:49:33 +0700

    add Entity position -close#211

--- backend/demo/src/main/java/lab3/demo/Entity/Position.java
+++ backend/demo/src/main/java/lab3/demo/Entity/Position.java
@@ -2,6 +2,11 @@ package lab3.demo.Entity;
 
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Date;
 @Data
 @Entity
@@ -11,10 +16,17 @@ import java.util.Date;
 @EqualsAndHashCode
 @Table(name="Position")
 public class Position { 
-    @Id @GeneratedValue
+    
+    @Id 
+    @SequenceGenerator(name="position_seq",sequenceName="position_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="position_seq")
+    @Column(name="positionId",unique = true, nullable = false)
+    @NotNull
     private Long id;
-
-    String name;
+    
+    @Size(min = 3,max = 20) 
+    @Pattern(regexp ="\\w+")
+    private @NotNull String name;
     
 
 

97646b65d8608dcd6ce79f3d51b61fcf4b80d5b1
jirayut1233 <jirayoot_19pop@hotmail.com>
2019-03-07 22:49:13 +0700

    add Entity discount -close#211

--- backend/demo/src/main/java/lab3/demo/Entity/Discount.java
+++ backend/demo/src/main/java/lab3/demo/Entity/Discount.java
@@ -2,6 +2,8 @@ package lab3.demo.Entity;
 
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -15,24 +17,31 @@ import java.util.Date;
 @Table(name="Discount")
 public class Discount {
 
-    @Id
-    @GeneratedValue
+    @Id 
+    @SequenceGenerator(name="discount_seq",sequenceName="discount_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="discount_seq")
+    @Column(name="discountId",unique = true, nullable = false)
+    @NotNull
     private Long id;
 
-    @ManyToOne
-    private TimeRange TimeRange;
-    @ManyToOne
-    private Admin admin;
-    @ManyToOne
-    private CarType carType;
+
+    
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Admin.class)
+    private  @NotNull Admin admin;
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = CarType.class)
+    private  @NotNull CarType carType;
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = TimeRange.class)
+    private  @NotNull TimeRange timeRange;
 
     @Size(min = 3,max = 20) @NotNull
     @Pattern(regexp ="\\w+")
     private  String promotion;
 
     @Temporal(TemporalType.DATE)
-    private  Date date;
+    private @NotNull Date date;
 
+    @Min(1)
+    @Max(999999)
     private  Integer price;
     
 

9a58e555f5ea1a832c4ddc7c029c6d350c108e32
anuttapon <balck_star1@hotmail.com>
2019-03-07 19:46:59 +0700

    add test case in Gender - close #209

--- backend/demo/src/main/java/lab3/demo/Entity/Gender.java
+++ backend/demo/src/main/java/lab3/demo/Entity/Gender.java
@@ -5,6 +5,9 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -17,6 +20,8 @@ public class Gender {
     @Id @GeneratedValue
     private Long id;
 
-    String sex;
+    @Pattern(regexp = "[WM]\\w+")
+    @Size(min = 0, max = 10)
+    @NotNull  private String sex;
 
 }

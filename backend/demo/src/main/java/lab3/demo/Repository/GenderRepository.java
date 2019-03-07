2b121600ce8c7b9a29fa26c5af4d7c0f6364b6fe
nicecpe2105 <channarong_seeho@hotmail.com>
2019-03-07 16:02:51 +0700

    แก้ไข Repository Gender -close #203

--- backend/demo/src/main/java/lab3/demo/Repository/GenderRepository.java
+++ backend/demo/src/main/java/lab3/demo/Repository/GenderRepository.java
@@ -7,4 +7,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface GenderRepository extends JpaRepository<Gender, Long>{
+    Gender findBysex(String sex);
 }
